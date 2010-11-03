64fb18b424b9b24f13ca5e5e230648a1f54d312d
Ryan Hsu <ryanlives@gmail.com>
2010-11-03 16:12:27 -0700

    added get_records in search post handler

--- controllers.py
+++ controllers.py
@@ -1,4 +1,5 @@
 import web
+from interface.risconnector import RISConnector
 
 render = web.template.render('templates/', base='base')
 
@@ -7,22 +8,27 @@ class index:
         return render.index()
 
 class options:
-    def GET(self):
+    def GET(self):        
         return render.options()
 		
-class search():
-	"""gets the search variables, searches for associated records,
-	processes the records through the icd-cpt engine and returns results
-	"""
-	def POST(self):
-		#process search vars
-		i = web.input()
-		
-		#get list of records
-		
-		#process the records
-		
-		#assemble return format
-		
-		#return results
-		return i.criteria_status
+class search:
+    """gets the search variables, searches for associated records,
+    processes the records through the icd-cpt engine and returns results
+    """
+    def POST(self):
+        # process search vars
+        i = web.input()
+        
+        start = i.criteria_start
+        end = i.criteria_end
+        status = i.criteria_status
+        
+        # get records
+        records = RISConnector().get_records(start, end)
+        
+        # process records
+        
+        # assemble return format
+
+        # FIXME: change return
+        return records
