2c06e5349fe26bc11ec210a5f917fe823451d94b
Ryan Hsu <ryanlives@gmail.com>
2010-11-03 16:09:47 -0700

    added get_records()

--- interface/risconnector.py
+++ interface/risconnector.py
@@ -1,12 +1,18 @@
-#need to enable connection pooling
-#db = web.database(dbn='mysql', db='name', user='aoeu')
+from sqlalchemy import and_
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.rismodel import Study, engine
 
 class RISConnector:
-	"""connects to the RIS system and provides methods for extracting data"""
-	def __init__(self, arg):
-		pass
-		
-	def get_records(self, start_date, end_date, status=""):
-		"""queries RIS for records based on date range between start_date and
-		end_date and optionally, status"""
-		pass
+    def get_records(self, start_date, end_date):
+        """queries RIS for records based on date range between start_date and
+        end_date"""
+        
+        session = scoped_session(sessionmaker(bind=engine))
+        
+        if start_date == end_date:
+            start_date += ' 00:00:00'
+            end_date += ' 24:59:59'
+            
+        records = session.query(Study).filter(and_(Study.date > start_date, Study.date < end_date)).all()
+
+        return records
