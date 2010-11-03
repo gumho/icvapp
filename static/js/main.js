d5dbf385e34cbfaaaec443790dec47a58b80990f
Ryan Hsu <ryanlives@gmail.com>
2010-11-03 15:55:21 -0700

    added function for converting datepicker date to sql date upon submit

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,22 @@
 var rowHeight = 26;
 
+function sqlDate(date) {
+    return date.substr(6,10) + '-' + date.substr(0,2) + '-' + date.substr(3,2);
+}
+
 $(document).ready(function() {
 	//option defaults
     $("#criteria_start, #criteria_end").datepicker({
 		defaultDate: -5,
-        maxDate: 0                
+        maxDate: 0,
+
     });
 
 	$("#criteria_start, #criteria_end").datepicker('setDate', new Date());
 	
 	//search submit handler
 	$("#search_button").click(function() {
-		var start = $('#criteria_end').val();
+		var start = $('#criteria_start').val();
 		var end = $('#criteria_end').val();
 		var status = $("input['criteria_status']:checked").val();
 		
@@ -19,10 +24,13 @@ $(document).ready(function() {
 			type: 'post',
 			url: '/search',
 			data: {
-				'criteria_start' : start,
-				'criteria_end' : end,
+				'criteria_start' : sqlDate(start),
+				'criteria_end' : sqlDate(end),
 				'criteria_status' : status
 			},
+            beforeSend: function() {
+                //TODO: validation
+            },
 			success: function(data) {
 				//append results to table
 				alert(data);
