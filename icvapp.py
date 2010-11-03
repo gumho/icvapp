6d8f3718ca5c59f99b6ca6c30082e39c558dccd3
Ryan Hsu <ryanlives@gmail.com>
2010-11-03 13:44:26 -0700

    changed app.runcgi() with .run() since app engine isn't being used anymore

--- icvapp.py
+++ icvapp.py
@@ -1,13 +1,16 @@
 import web
 
+from processors import load_sqla
 from controllers import *
 
 urls = (
 	'/', 'index',
 	'/options', 'options',
 	'/search', 'search',
+	'/faq', 'faq'
 )
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
-    app.cgirun()
+    app.add_processor(load_sqla)
+    app.run()
