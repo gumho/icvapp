136c61790a9b1b1b5cd6b3206b2afc1bcc8872cf
Ryan Hsu <ryanlives@gmail.com>
2010-11-07 21:13:57 -0800

    implemented checkRecords

--- interface/crossconnector.py
+++ interface/crossconnector.py
@@ -1,14 +1,10 @@
+from vendor import CrossChecker
+
 class CrossConnector:
 	"""this module checks the icd-cpt database for validating codes"""
-	def __init__(self, arg):
-		pass
 
-	def checkRecords(self):
+	def checkRecords(self, icd, cpt):
 		"""takes a list of records and batch checks them. returns list 
 		with results.
 		"""
-		pass
-		
-	def _check_pair(self):
-		"""docstring for _check_pair"""
-		pass
+		return CrossChecker.check_pair(icd, cpt)
