3db6bcad3915644429ac42ed7b3c2b09d576a65a
Ryan Hsu <ryanlives@gmail.com>
2010-11-07 21:16:31 -0800

    renamed function

--- interface/crossconnector.py
+++ interface/crossconnector.py
@@ -3,7 +3,7 @@ from vendor import CrossChecker
 class CrossConnector:
 	"""this module checks the icd-cpt database for validating codes"""
 
-	def checkPairs(self, code_pair_list):
+	def validatePairs(self, code_pair_list):
 		"""takes a list of records and batch checks them. returns list 
 		with results.
 		"""
