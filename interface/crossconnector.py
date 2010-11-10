7ceb239bf522186083c0c13c1332651bd80368ec
Ryan Hsu <ryanlives@gmail.com>
2010-11-09 22:00:56 -0800

    added single pair validate method

--- interface/crossconnector.py
+++ interface/crossconnector.py
@@ -1,10 +1,15 @@
-from vendor import CrossChecker
+from vendor.crosschecker import CrossChecker
 
 class CrossConnector:
-	"""this module checks the icd-cpt database for validating codes"""
-
+    """This module checks the icd-cpt database for validating codes. 
+    Returns true if good code pair."""
+    
+    def validate(self, icd, cpt):
+        checker = CrossChecker()
+        return checker.checkPair(icd, cpt)
+        
 	def validatePairs(self, code_pair_list):
-		"""takes a list of records and batch checks them. returns list 
-		with results.
+		"""Takes a list of records and batch checks them. returns list 
+		with results. Returs false if not.
 		"""
         pass
