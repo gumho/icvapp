7d10c80e9853b5500284353f5d1b0a86178f4777
Ryan Hsu <ryanlives@gmail.com>
2010-11-16 20:14:32 -0800

    added ui, fixed ajax handler

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,63 @@
-var rowHeight = 26;
-
 //takes '11/8/2010' and turns into '2010-11-08'
 function sqlDate(date) {
     return date.substr(6,10) + '-' + date.substr(0,2) + '-' + date.substr(3,2);
 }
 
+
+
 $(document).ready(function() {
+    //--------UI--------
+    
+    //accordion
+    $("#results ul li h3").click(function() {
+       $(this).next().toggle('slow'); 
+    });
+    
+    //search button
+    $("#search_btn").button();
+    
+    //more options
+    $("#more_options_btn").toggle(function() {
+       $(this).html('Less options'); 
+    }, function() {
+       $(this).html('More options');        
+    });
+    
 	//option defaults
-    $("#criteria_start, #criteria_end").datepicker({
+    $("#begin_date, #end_date").datepicker({
 		defaultDate: -5,
         maxDate: 0,
-
+        showOn: "button",
+		buttonImage: "/static/images/cal.png",
+		buttonImageOnly: true
     });
-
-	$("#criteria_start, #criteria_end").datepicker('setDate', new Date());
+    
+    //set today's date
+	$("#begin_date, #end_date").datepicker('setDate', new Date());
+	
+	//show search options
+	$("#more_options_btn").click(function() {
+	    $("#options").slideToggle();
+	});
+	
+	//-------CORE-----
 	
 	//search submit handler
-	$("#search_button").click(function() {
-		var start = $('#criteria_start').val();
-		var end = $('#criteria_end').val();
-		var status = $("input['criteria_status']:checked").val();
+	$("#search_btn").click(function() {
+		var start = $('#begin_date').val();
+		var end = $('#end_date').val();
+		var status = $('input:checked').map(function(i,n) {
+            return $(n).val();
+        }).get();
 		
 		$.ajax({
 			type: 'post',
 			url: '/search',
+			traditional: true,
 			data: {
-				'criteria_start' : sqlDate(start),
-				'criteria_end' : sqlDate(end),
-				'criteria_status' : status
+				'begin_date' : sqlDate(start),
+				'end_date' : sqlDate(end),
+				'status' : status
 			},
             beforeSend: function() {
                 //TODO: validation
@@ -42,23 +72,7 @@ $(document).ready(function() {
 		
 		return false;
 	});
-	
-	//result drop click handlers
-	$("#results tr").live('click', function() {
-		var topOffset = $(this).offset().top + rowHeight - 1;
-		
-		$("#result_drop").css({
-			'top': topOffset + 'px'
-			// 'display': 'block'
-		});
-		
-		$("#result_drop").slideDown();
-	}
-    );
-	
-	$("#rd_close").click(function() {
-		$("#result_drop").slideUp();
-		return false;
-	});
+
+    
 
 })
