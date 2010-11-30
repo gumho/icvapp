9c9926e24e1337ac549068200be9e28165459502
Ryan Hsu <ryanlives@gmail.com>
2010-11-30 00:14:52 -0800

    initial commit

---
+++ utils/dateutils.py
@@ -0,0 +1,52 @@
+import datetime
+
+now = datetime.datetime.now()
+
+months = {
+    '1':'Jan',
+    '2':'Feb',
+    '3':'Mar',
+    '4':'Apr',
+    '5':'May',
+    '6':'Jun',
+    '7':'Jul',
+    '8':'Aug',
+    '9':'Sep',
+    '10':'Oct',
+    '11':'Nov',
+    '12':'Dec',
+}
+
+def sql_datetime_to_human_date(datetime):
+    """converts sql datetime to human readable date. ex. 2010-11-29 10:30:15 => Nov 29"""
+    m = months[datetime[5:7]]
+    d = datetime[8:10]
+    y = datetime[0:4]
+    
+    if(y == str(now.year)):
+        y = ''
+    else:
+        y = ', %s' % y
+    
+    return ''.join([m, ' ', d, y])    
+
+def sql_datetime_to_12hr_time(datetime):
+    """converts sql datetime to human readable time. ex. 2010-11-29 13:30:15 => 1:30 PM"""
+    h = to12(datetime[11:13])
+    m = datetime[14:16]
+
+    if(h > 12): 
+        suf = 'PM'
+    else: 
+        suf = 'AM'
+    
+    return ''.join([h, ':', m, ' ', suf])
+    
+def to12(hour24):
+    return str((int(hour24) % 12) if (int(hour24) % 12) > 0 else 12)
+
+    
+if __name__ == '__main__':
+    # print sql_datetime_to_human_date('2010-11-29 10:30:15')
+    print sql_datetime_to_12hr_time('2010-11-29 01:40:15')
+    # print to12(10)
